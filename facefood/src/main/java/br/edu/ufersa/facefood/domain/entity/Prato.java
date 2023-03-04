354bcc90a5f77c4e75e00c4a80e4695cd631aec4
Ryan Borges <ryanborgeslockser@gmail.com>
2023-03-04 18:55:40 -0300

    add: entidade Prato

--- facefood/src/main/java/br/edu/ufersa/facefood/domain/entity/Prato.java
+++ facefood/src/main/java/br/edu/ufersa/facefood/domain/entity/Prato.java
@@ -3,23 +3,69 @@ package br.edu.ufersa.facefood.domain.entity;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
+@Entity
+@Table(name = "tb_pratos")
 public class Prato extends Ingrediente {
-	private List<Ingrediente> listaIngredientes = new ArrayList<Ingrediente>();
-	private int idDoCriador;
 	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+	@Column(nullable=false)
+	private String nome;
+	private float calorias;
+	private List<Ingrediente> listaIngredientes = new ArrayList<Ingrediente>();
+	private List<String> tipos = new ArrayList<String>();
+	private String descricao;
+	private User autor;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public float getCalorias() {
+		return calorias;
+	}
+	public void setCalorias(float calorias) {
+		this.calorias = calorias;
+	}
 	public List<Ingrediente> getListaIngredientes() {
 		return listaIngredientes;
 	}
 	public void setListaIngredientes(List<Ingrediente> listaIngredientes) {
 		this.listaIngredientes = listaIngredientes;
 	}
-	public int getIdDoCriador() {
-		return idDoCriador;
+	public List<String> getTipos() {
+		return tipos;
 	}
-	public void setIdDoCriador(int idDoCriador) {
-		this.idDoCriador = idDoCriador;
+	public void setTipos(List<String> tipos) {
+		this.tipos = tipos;
 	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public User getAutor() {
+		return autor;
+	}
+	public void setAutor(User autor) {
+		this.autor = autor;
+	}
+	
 }
