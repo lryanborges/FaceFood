a46bc348ccb320375208823a1bbd8adc2bc15d43
João Sales <joaosales911@gmail.com>
2023-03-06 21:22:41 -0300

    ajeitando a api

--- facefood/src/main/java/br/edu/ufersa/facefood/api/dto/UpdateRefeicaoDTO.java
+++ facefood/src/main/java/br/edu/ufersa/facefood/api/dto/UpdateRefeicaoDTO.java
@@ -1,35 +1,41 @@
 package br.edu.ufersa.facefood.api.dto;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
 import java.util.UUID;
 
-import javax.validation.constraints.NotEmpty;
-
 import br.edu.ufersa.facefood.domain.entity.Prato;
+import br.edu.ufersa.facefood.domain.entity.Rotina;
 
 public class UpdateRefeicaoDTO {
-	@NotEmpty(message="Coloque um horario atrelado a refeição")
-	 private LocalDateTime horario;
-	@NotEmpty(message="Coloque um prato atrelado a refeição")
-	 private Prato prato;
-	private UUID uuid;
-	public LocalDateTime getHorario() {
-		return horario;
-	}
-	public void setHorario(LocalDateTime horario) {
-		this.horario = horario;
-	}
-	public Prato getPrato() {
-		return prato;
-	}
-	public void setPrato(Prato prato) {
-		this.prato = prato;
-	}
-	public UUID getUuid() {
-		return uuid;
-	}
-	public void setUuid(UUID uuid) {
-		this.uuid = uuid;
-	}
+	 private LocalTime horario;
+	    private List<Prato> pratos;
+		private UUID uuid;
+	    private Rotina rotina;
+		public LocalTime getHorario() {
+			return horario;
+		}
+		public void setHorario(LocalTime horario) {
+			this.horario = horario;
+		}
+		public List<Prato> getPratos() {
+			return pratos;
+		}
+		public void setPratos(List<Prato> pratos) {
+			this.pratos = pratos;
+		}
+		public UUID getUuid() {
+			return uuid;
+		}
+		public void setUuid(UUID uuid) {
+			this.uuid = uuid;
+		}
+		public Rotina getRotina() {
+			return rotina;
+		}
+		public void setRotina(Rotina rotina) {
+			this.rotina = rotina;
+		}
 	
 }
--- facefood/src/main/java/br/edu/ufersa/facefood/domain/entity/Refeicao.java
+++ facefood/src/main/java/br/edu/ufersa/facefood/domain/entity/Refeicao.java
@@ -29,7 +29,8 @@ public class Refeicao {
     
     @ManyToMany
     @JoinColumn(name = "id")
-    private Prato prato;
+    private List<Prato> pratos;
+    //private Prato prato;
     
     @Column(updatable = false, nullable = false, columnDefinition = "VARCHAR(36)")
 	@Type(type = "uuid-char")
@@ -48,12 +49,6 @@ public class Refeicao {
 	public Refeicao() {
     }
     
-    public Refeicao(LocalTime horario, Prato prato, UUID uuid) {
-        this.horario = horario;
-        this.prato = prato;
-        this.uuid = uuid;
-    }
-    
     public Long getId() {
         return id;
     }
@@ -70,14 +65,6 @@ public class Refeicao {
         this.horario = horario;
     }
     
-    public Prato getPrato() {
-        return prato;
-    }
-    
-    public void setPrato(Prato prato) {
-        this.prato = prato;
-    }
-    
     public UUID getUuid() {
         return uuid;
     }
@@ -85,24 +72,42 @@ public class Refeicao {
     public void setUuid(UUID uuid) {
         this.uuid = uuid;
     }
-    
-    @Override
-	public int hashCode() {
-    	return Objects.hash(id, horario, prato, uuid);
+
+	public List<Prato> getPratos() {
+		return pratos;
 	}
-	
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Refeicao other = (Refeicao) obj;
-        return Objects.equals(id, other.id) &&
-               Objects.equals(horario, other.horario) &&
-               Objects.equals(prato, other.prato) &&
-               Objects.equals(uuid, other.uuid);
-    }
+
+	public void setPratos(List<Prato> pratos) {
+		this.pratos = pratos;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(horario, id, pratos, rotina, uuid);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Refeicao other = (Refeicao) obj;
+		return Objects.equals(horario, other.horario) && Objects.equals(id, other.id)
+				&& Objects.equals(pratos, other.pratos) && Objects.equals(rotina, other.rotina)
+				&& Objects.equals(uuid, other.uuid);
+	}
+
+	public Refeicao(Long id, LocalTime horario, List<Prato> pratos, UUID uuid, Rotina rotina) {
+		super();
+		this.id = id;
+		this.horario = horario;
+		this.pratos = pratos;
+		this.uuid = uuid;
+		this.rotina = rotina;
+	}
+    
+ 
 }
