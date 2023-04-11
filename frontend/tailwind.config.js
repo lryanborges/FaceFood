26ea615531cfc3db49de4888c8bc4af1fc51e070
Caio <102376017+Caio-T4vares@users.noreply.github.com>
2023-04-11 20:50:45 -0300

    header e desc
    ;

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,14 +4,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        grayish: '#3D3D3D',
-        red: '#ff0038',
-        cinza: '#EDEDED',
-        brancoamarelado: 'FFFCF9',
-        preto: '1A1A1A'
-      }
-    }
+        grayish: "#3D3D3D",
+        red: "#ff0038",
+        cinza: "#EDEDED",
+        brancoamarelado: "FFFCF9",
+        preto: "1A1A1A",
+        cinzaTexto: "7C7878",
+      },
+    },
   },
   plugins: [],
-}
-
+};
